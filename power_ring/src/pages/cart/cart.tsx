1f55e29f6f3912b0d0a51b207a39d5de6f39636b
Gustavo Gabriel <trezmanos@gmail.com>
2023-11-23 21:13:43 -0300

    Mad changes to cart to show products and price

--- power_ring/src/pages/cart/cart.tsx
+++ power_ring/src/pages/cart/cart.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { GContext } from '../../contexts/gcontext';
 
@@ -8,15 +8,34 @@ import './cart.css';
 
 export function Cart(){
 
-    const {chosens, setChosens} = useContext(GContext)
+    const {chosens, setChosens, ChangeChosenValue, HowMany} = useContext(GContext)
+
+    const [totalProducts, setTotalProducts] = useState<any>()
+    const [totalPrice, setTotalPrice] = useState(0)
+
+    function updateTotalProducts(){
+        const productList = products.filter( i => chosens.some(item => item.ID == i.ID))
+        const temp = productList.map(item => item.name)
+        setTotalProducts(temp.join(', '))
+    }
+    function  updateTotalPrice(){
+        const productList = products.filter( i => chosens.some(item => item.ID == i.ID))
+        const temp = productList.map(item => item.price*HowMany(item.ID))
+
+        let Sum = 0
+
+        temp.forEach(item => {Sum = item + Sum})
+
+        setTotalPrice(Sum)
+    }
+
+    useEffect(() => {updateTotalProducts(); updateTotalPrice()}, [chosens])
 
     return(
         <div className='Cart'>
-            {products.filter(product => chosens.some( item => item.ID == product.ID)).map(product => <>
-            
-            <h1 key={product.ID}>{product.name}</h1>
-
-            </>)}
+            <h1>Items in cart:</h1>
+            <p>{totalProducts}</p>
+            <p>{totalPrice}</p>
             <Link to='/'><button></button></Link>
         </div>
     )
